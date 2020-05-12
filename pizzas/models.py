fb61c5c7a0b4b47df9a064b292594f2455881e4e
Zack Snider <zacksnider@Zacks-MacBook-Pro.local>
2020-05-12 17:09:06 -0500

    registered Pizza and Topping model, added pizzas

--- pizzas/models.py
+++ pizzas/models.py
@@ -9,3 +9,11 @@ class Pizza(models.Model):
     def __str__(self):
         return self.name
 
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name[:50]}..."
+    
